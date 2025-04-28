fdc30d01f0de686d4d8d9d52338a6602a03fceba
Matin <mkomijani323@yahoo.com>
2025-04-28 23:56:43 +0330

    feat: design country page(1)

--- src/app/countries/[id]/page.tsx
+++ src/app/countries/[id]/page.tsx
@@ -15,12 +15,52 @@ export async function generateStaticParams() {
 }
 export default async function page({ params }: { params: { id: string } }) {
     const res = await fetch(`http://localhost:3000/countries/${params.id}`);
-    const countries = await res.json();
+    const country = await res.json();
 
     return (
         <div>
-
-
+            <main className="flex justify-start">
+                {/* country flag section */}
+                <section className="w-max">
+                    
+                </section>
+                {/* country details section */}
+                <section>
+                    <h1 className="text-3xl font-bold mb-4">{country.name}</h1>
+                    <ul className="list-none space-y-2">
+                        <li>
+                            <strong>Native Name:</strong> {country.nativeName}
+                        </li>
+                        <li>
+                            <strong>Population:</strong> {country.population.toLocaleString()}
+                        </li>
+                        <li>
+                            <strong>Region:</strong> {country.region}
+                        </li>
+                        <li>
+                            <strong>Sub Region:</strong> {country.subRegion}
+                        </li>
+                        <li>
+                            <strong>Capital:</strong> {country.capital}
+                        </li>
+                        <li>
+                            <strong>Top Level Domain:</strong> {country.topLevelDomain.join(", ")}
+                        </li>
+                        <li>
+                            <strong>Currencies:</strong> {country.currencies.join(", ")}
+                        </li>
+                        <li>
+                            <strong>Languages:</strong> {country.languages.join(", ")}
+                        </li>
+                        <li>
+                            <strong>Border Countries:</strong>{" "}
+                            {country.borders.length > 0
+                                ? country.borders.join(", ")
+                                : "None"}
+                        </li>
+                    </ul>
+                </section>
+            </main>
         </div>
     )
 }
