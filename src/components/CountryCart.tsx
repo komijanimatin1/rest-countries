30ff45847b0590ed289b1b4f4023233180b43aa7
Mohammad Matin <mkomijani118@gmail.com>
2025-05-02 21:29:03 +0330

    feat: wraps country cart in a Link component

--- src/components/CountryCart.tsx
+++ src/components/CountryCart.tsx
@@ -2,29 +2,31 @@ import Link from "next/link";
 
 export default function CountryCart({ id, name, population, region, capital, flag }: { id: string; name: string; population: number; region: string; capital: string; flag: string; }) {
     return (
-        <div className="bg-white rounded-lg shadow-md overflow-hidden w-80 my-4 hover:scale-105 active:scale-95 transition-all duration-200">
-            <img
-                src={flag}
-                alt={`${name} flag`}
-                className="w-full h-40 object-cover س"
-            />
-            <div className="p-4">
-                <Link
-                    href={`/countries/${id}`}
-                    className="text-lg font-bold mb-2"
-                >
-                    {name}
-                </Link>
-                <p className="text-sm mb-1">
-                    <span className="font-semibold">Population:</span> {population.toLocaleString()}
-                </p>
-                <p className="text-sm mb-1">
-                    <span className="font-semibold">Region:</span> {region}
-                </p>
-                <p className="text-sm">
-                    <span className="font-semibold">Capital:</span> {capital}
-                </p>
+        <Link
+            href={`/countries/${id}`}
+            className="text-lg font-bold mb-2"
+        >
+            <div className="bg-white rounded-lg shadow-md overflow-hidden w-80 my-4 hover:scale-105 active:scale-95 transition-all duration-200">
+                <img
+                    src={flag}
+                    alt={`${name} flag`}
+                    className="w-full h-40 object-cover س"
+                />
+                <div className="p-4">
+                    <h2>
+                        {name}
+                    </h2>
+                    <p className="text-sm mb-1">
+                        <span className="font-semibold">Population:</span> {population.toLocaleString()}
+                    </p>
+                    <p className="text-sm mb-1">
+                        <span className="font-semibold">Region:</span> {region}
+                    </p>
+                    <p className="text-sm">
+                        <span className="font-semibold">Capital:</span> {capital}
+                    </p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
