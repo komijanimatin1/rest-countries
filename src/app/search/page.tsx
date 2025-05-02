c551ac5d1245ab805ed8a51cd595c402f4e8aef5
Mohammad Matin <mkomijani118@gmail.com>
2025-05-02 18:38:18 +0330

    refactor: refactor data fetching for search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,31 +1,13 @@
 import CountriesSection from "@/components/CountriesSection";
-import axios from "axios";
-
-type Country = {
-  id: string;
-  name: string;
-  nativeName: string;
-  population: number;
-  region: string;
-  subRegion: string;
-  capital: string;
-  topLevelDomain: string[];
-  currencies: string[];
-  languages: string[];
-  borders: string[];
-  flag: string;
-};
+import { getCountries } from "../libraries/getCountries";
 
 export default async function SearchPage({ searchParams }: { searchParams: { query?: string } }) {
   const query = searchParams.query?.toLowerCase() || '';
-
-  const { data: countries } = await axios.get<Country[]>('http://localhost:3000/countries', {
-    headers: { 'Content-Type': 'application/json' },
-  });
+  const countries = await getCountries();
 
   const filtered = countries.filter((country) => {
     const queryLower = query.toLowerCase();
-  
+
     const searchableContent = `
       ${country.name}
       ${country.nativeName}
@@ -37,10 +19,10 @@ export default async function SearchPage({ searchParams }: { searchParams: { que
       ${country.languages.join(" ")}
       ${country.borders.join(" ")}
     `.toLowerCase();
-  
+
     return searchableContent.includes(queryLower);
   });
-  
+
   return (
     <main>
       <h1 className="text-center text-2xl font-bold mt-10">
