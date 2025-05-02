d9a8086286211ab387ab39de4dd76a7cf8c27791
Mohammad Matin <mkomijani118@gmail.com>
2025-05-02 17:55:55 +0330

    fix: edit import problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import CountriesSection from "@/components/CountriesSection";
 import Navbar from "@/components/Navbar";
-import axios from "axios";
 import { getCountries } from "./libraries/getCountries";
+import SearchComponent from "@/components/Searchbar";
 
 export default async function Home() {
 
   const countries = await getCountries();
   return (
     <div>
-        <Navbar/>
-        <SearchComponent/>
-        <CountriesSection countries={countries}/>
-        
+      <Navbar />
+      <SearchComponent />
+      <CountriesSection countries={countries} />
+
     </div>
   );
 }
